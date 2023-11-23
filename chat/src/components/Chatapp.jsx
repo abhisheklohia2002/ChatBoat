fdbf448859ad275c7980edb732bd974066900627
akshaylohia <akshaylohia056@gmail.com>
2023-11-23 15:19:04 +0530

    added

--- chat/src/components/Chatapp.jsx
+++ chat/src/components/Chatapp.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import "./chat.css"
-const socket = io('http://localhost:8080'); 
+const socket = io('https://chat-boat-three.vercel.app/'); 
 
 function ChatApp() {
   const [chatbot, setChatbot] = useState([]);
